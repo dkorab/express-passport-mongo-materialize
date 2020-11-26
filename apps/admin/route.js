c6638b4279c377f5b7f907ef723d20f25dd24b62
dkorab <dkorab@users.noreply.github.com>
2020-11-26 13:47:23 +0100

    Admin login/register view

--- apps/admin/route.js
+++ apps/admin/route.js
@@ -2,19 +2,21 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// /* GET admin home page. */
-// router.get('/', function(req, res, next) {
-//   res.render(path.join(__dirname,'views', 'index'), { title: 'Admin' });
-// });
+/* GET admin home page. */
+router.get('/', function(req, res, next) {
+  res.send('Admin');
+});
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
-  res.render(path.join(__dirname,'views', 'login'), {layout: path.join(__dirname,'views', 'layouts', 'admin'), pageCss: '/css/pages/admin/login.css'});
+  res.render(path.join(__dirname,'views', 'login'),
+      {layout: path.join(__dirname,'views', 'layouts', 'admin'), pageCss: '/css/pages/admin/login.css', pageTitle: 'Login'});
 });
 
-// /* GET register page. */
-// router.get('/register', function(req, res, next) {
-//   res.send('register');
-// });
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render(path.join(__dirname,'views', 'register'),
+      {layout: path.join(__dirname,'views', 'layouts', 'admin'), pageCss: '/css/pages/admin/login.css', pageTitle: 'Register'});
+});
 
 module.exports = router;
