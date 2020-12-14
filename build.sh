70423a7c742889e4576bf6e4bc3551b78557c2ae
dkorab <dkorab@users.noreply.github.com>
2020-12-14 11:30:22 +0100

    Fixes in js generator

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # Build js file for specific page or for all
 
 if [ "$#" -eq 0 ] || [ "$1" = 'login' ]; then
-  browserify ./js/pages/admin/login -p tinyify -o ./public/js/pages/admin/login.js
-  echo 'Login JS file generated (./public/js/pages/admin/register.js).'
+#  browserify ./js/pages/admin/login --noparse="$PWD/js/common/materialize.min.js" -p tinyify -o ./public/js/pages/admin/login.js
+#faster:
+  { cat ./js/common/materialize.min.js; browserify ./js/pages/admin/login -p tinyify; } > ./public/js/pages/admin/login.js
+  echo 'Login JS file generated (./public/js/pages/admin/login.js).'
 fi
 
 if [ "$#" -eq 0 ] || [ "$1" = 'register' ]; then
-  browserify ./js/pages/admin/register -p tinyify -o ./public/js/pages/admin/register.js
+  { cat ./js/common/materialize.min.js; browserify ./js/pages/admin/register -p tinyify; } > ./public/js/pages/admin/register.js
   echo 'Register JS file generated (./public/js/pages/admin/register.js).'
 fi
--- js/pages/admin/register.js
+++ js/pages/admin/register.js
@@ -1,5 +1,5 @@
 'use strict';
 
-require('../../common/materialize.min');
+// require('../../common/materialize.min'); // depends from browserify options
 require('../../common/fix');
 require('../../elements/starter')('/css/pages/admin/register.css');
